96b2e1d43a9343736da105e207cf87bb94737869
Qiratsumra <saeedqirat43@gmail.com>
2025-03-03 16:54:05 +0500

    PYTHON_CHALLENGE_DAILY

--- day_02.py
+++ day_02.py
@@ -2,4 +2,6 @@ sentence='Python is a versatile, high-level programming language known for its s
 
 split_words= sentence.split()
 total_words= len(split_words)
+reverse_word= split_words[::-1]
 print(f'total word in this \"{sentence}\" sentence is\n \"{total_words}\"')
+print(reverse_word)

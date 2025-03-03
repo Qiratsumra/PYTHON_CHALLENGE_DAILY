51c4f6305fb3423621301b940f8a9beebb8966ed
Qiratsumra <saeedqirat43@gmail.com>
2025-03-03 16:46:59 +0500

    PYTHON_CHALLENGE_DAILY

---
+++ day_02.py
@@ -0,0 +1,5 @@
+sentence='Python is a versatile, high-level programming language known for its simplicity, readability, and powerful libraries, making it a popular choice for web development, data science, automation, and artificial intelligence. 🚀'
+
+split_words= sentence.split()
+total_words= len(split_words)
+print(f'total word in this \"{sentence}\" sentence is\n \"{total_words}\"')
